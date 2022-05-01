e6c8f0b326e09a1074952257e55bc9ab5949669f
Roberto Hernandez <robhdr588@gmail.com>
2022-05-01 14:36:32 -0500

    Avance del proyecto de la API

---
+++ Proyecto_API/test.py
@@ -0,0 +1,72 @@
+import unittest
+from unittest import mock
+from unittest.mock import patch
+from DB import *
+from API import *
+
+class Tests(unittest.TestCase):
+    
+    @patch('DB.ConexionBD.eliminar_equipo', return_value = True)
+    def test_eliminar_equipo(self, mock):
+        bd = ConexionBD()
+        expected = True
+        actual = bd.eliminar_equipo(1)
+        assert expected == actual
+    
+    
+    @patch('DB.ConexionBD.eliminar_equipo', return_value = False)
+    def test_eliminar_equipo_dos(self,mock):
+        bd = ConexionBD()
+        resultado_esperado = False
+        resultado_actual = bd.eliminar_equipo(2)
+        assert resultado_esperado == resultado_actual 
+    
+    
+    @patch('DB.ConexionBD.eliminar_pokemon', return_value = True)
+    def test_eliminar_pokemon(self,mock):
+        bd = ConexionBD()
+        resultado_esperado = True
+        resultado_actual = bd.eliminar_pokemon(5)
+        assert resultado_esperado == resultado_actual
+    
+    
+    @patch('DB.ConexionBD.eliminar_pokemon', return_value = False)
+    def test_eliminar_pokemon_dos(self,mock):
+        bd = ConexionBD()
+        resultado_esperado = False
+        resultado_actual = bd.eliminar_pokemon(5)
+        assert resultado_esperado == resultado_actual
+    
+    
+    @patch('DB.ConexionBD.guardar_equipo', return_value = True)
+    def test_guardar_equipo(self,mock):
+        bd = ConexionBD()
+        expected = True
+        actual = bd.guardar_equipo("mock")
+        assert expected == actual
+        
+        
+    @patch('DB.ConexionBD.guardar_equipo', return_value = False)
+    def test_guardar_equipo_dos(self,mock):
+        bd = ConexionBD()
+        expected = False
+        actual = bd.guardar_equipo("mock")
+        assert expected == actual
+    
+
+    @patch('DB.ConexionBD.guardar_pokemon', return_value = True)
+    def test_guardar_pokemon(self,mock):
+        bd = ConexionBD()
+        expected = True
+        actual = bd.guardar_pokemon("mock",12)
+        assert expected == actual
+        
+    @patch('DB.ConexionBD.guardar_pokemon', return_value = False)
+    def test_guardar_pokemon_dos(self,mock):
+        bd = ConexionBD()
+        expected = False
+        actual = bd.guardar_pokemon("mock",12)
+        assert expected == actual
+    
+if __name__ == '__main__':
+    unittest.main()
